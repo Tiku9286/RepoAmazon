c08ad5a7d548d431b4bb5aae58b0f7b096579cdc
rahul <rahul@TIKUDELL-2011>
2023-02-20 11:57:43 +0530

    new class 20-02-2023

---
+++ git/src/gitDemo/Demogit11.java
@@ -0,0 +1,15 @@
+package gitDemo;
+
+public class Demogit11 {
+	public static void main(String[] args) {
+
+	
+	
+	
+		System.out.println("bye");
+		System.out.println("amol");
+	System.out.println("hello");
+
+
+}
+}
